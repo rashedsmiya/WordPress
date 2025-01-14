abd789a5f8da1d9f737db0b41d73f38e90c43f60
rashedsmiya <rashedmiyaofficial@gmail.com>
2025-01-14 10:36:44 +0600

    register_menu

--- wp_poblems/register_menu.php
+++ wp_poblems/register_menu.php
@@ -32,3 +32,31 @@ wp_nav_menu(array(
     'container'      => 'nav',    // HTML container tag (e.g., <nav>)
     'menu_class'     => 'primary-menu', // CSS class for the menu
 ));
+
+
+1. Register Nav menu 
+
+<?php 
+
+    register_nav_menus(array(
+        'header_menu' => 'Header Menu', 
+        'footer_menu' => 'Footer Menu'
+    ));
+
+    2. Create Page for menu 
+
+    3. echo menu and add style (Front End)
+
+        <?php wp_nav_menu(array(
+            'thmeme_location' => 'header_menu', 
+            'menu_id' => '', 
+            'menu_class' => 'nav navbar-nav', 
+            'container_class' => 'navbar-collapse collapse align-left', 
+            'container_id' => '', 
+            'depth' => '3', 
+            'fallback_cp' => 'wp_bootstrap_Navwalker::fallback', 
+            'walker'      => new WP_Bootstrap_NavWalker()  
+        ));?>
+
+    4. Add navwalker(github version) for dropwon support
+?>
