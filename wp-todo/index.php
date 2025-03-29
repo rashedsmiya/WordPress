420fea07c0cfdd9e526f030066f33a39b7ddc791
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2025-03-29 10:31:01 +0600

    add

---
+++ wp-todo/index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+     /*
+      *  plugin Name:        wp-todo plugin
+      *  plugin url:         https://wordpress.org/plugins/wp-todo/
+      *  Description:        A plugin to manage your todo list
+      *  Version:            1.0.0
+      *  Requires at least:  5.2
+      *  Requires PHP:       7.2
+      *  Author:             CleanCodeWithSumon
+      *  Author URI:         https://CleanCodeWithSumon.com/
+      *  License:            GPL v2 or later
+      *  License URL:        https://www.gnu.org/licenses/gpl-2.0.html
+      *  Text Domain:        wp-todo-plugin
+      *  Domain Path:        /languages
+      * */
+
+
+
+     
+
